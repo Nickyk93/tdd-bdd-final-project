6eca7146d28d63c5e2314a2ddcb5f9a095509e4f
Nicky <nicolelyndon@utilita.co.uk>
2025-01-19 14:33:18 +0000

    Test case to delete a product

--- tests/test_models.py
+++ tests/test_models.py
@@ -135,3 +135,13 @@ class TestProductModel(unittest.TestCase):
         self.assertEqual(len(products), 1)
         self.assertEqual(products[0].id, original_id)
         self.assertEqual(products[0].description, "testing")
+
+    def test_delete_a_product(self):
+        """It should Delete a Product"""
+        product = ProductFactory()
+        # Create a product
+        product.create()
+        self.assertEqual(len(Product.all()), 1)
+        # Delete the product and confirm deleted
+        product.delete()
+        self.assertEqual(len(Product.all()), 0)
